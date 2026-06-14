be12669557a4a7c102240f7e1fb41c1e17c9fd42
ibnaleem <ibnaleem@outlook.com>
2026-06-14 13:58:25 +0100

    feat: add cobra command to add a new habit

---
+++ cmd/add.go
@@ -0,0 +1,43 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/ibnaleem/cadence/internal/theme"
+	"github.com/ibnaleem/cadence/internal/util"
+	"github.com/spf13/cobra"
+)
+
+var addCmd = &cobra.Command{
+	Use:   "add <name>",
+	Short: "Add a new habit",
+	Args:  cobra.ExactArgs(1),
+
+	RunE: func(cmd *cobra.Command, args []string) error {
+		description, _ := cmd.Flags().GetString("description")
+		frequency, _ := cmd.Flags().GetString("frequency")
+
+		db, err := util.InitDB()
+		if err != nil {
+			return err
+		} // if
+		defer db.Close()
+
+		if err := util.SetupSchema(db); err != nil {
+			return err
+		} // if
+
+		if err := util.AddHabit(db, args[0], description, frequency); err != nil {
+			return err
+		} // if
+
+		fmt.Println(theme.Green("✓") + " habit added: " + theme.Bold(args[0]))
+
+		return nil
+	}, // RunE
+} // &cobra.Command
+
+func init() {
+	addCmd.Flags().StringP("description", "d", "", "optional description")
+	addCmd.Flags().StringP("frequency", "f", "daily", "daily or weekly")
+} // init
